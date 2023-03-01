e9c7ae084d4797920825f9ca1acafc1f8f130f70
daorongxing <12044046+yanqingzheng@user.noreply.gitee.com>
2023-03-01 16:04:51 +0800

    用户历史消费记录表业务逻辑层

---
+++ src/main/java/com/example/purse_demo/service/HistoryService.java
@@ -0,0 +1,22 @@
+package com.example.purse_demo.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.purse_demo.domain.History;
+import com.example.purse_demo.domain.response.ResponseData;
+
+/**
+* @author centos7
+* @description 针对表【history(历史交易明细表)】的数据库操作Service
+* @createDate 2023-03-01 11:19:39
+*/
+public interface HistoryService extends IService<History> {
+
+    /***
+     * @description 查询用户钱包金额变动明细
+     * @param uAccount 用户账号
+     * @return com.example.purse_demo.domain.response.ResponseData
+     * @author daorong
+     * @date 2023/3/1 14:13
+     */
+    ResponseData getHistory(String uAccount);
+}
