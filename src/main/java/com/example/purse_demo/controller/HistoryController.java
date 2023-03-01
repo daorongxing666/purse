093324bba8d6fb32d09ee41715b475764f5fae1a
daorongxing <12044046+yanqingzheng@user.noreply.gitee.com>
2023-03-01 16:01:19 +0800

    用户历史消费记录表控制层

---
+++ src/main/java/com/example/purse_demo/controller/HistoryController.java
@@ -0,0 +1,38 @@
+package com.example.purse_demo.controller;
+
+import com.example.purse_demo.domain.response.ResponseData;
+import com.example.purse_demo.service.HistoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author daorong
+ * @version 1.0
+ * @description TODO
+ * @date 2023/3/1 14:03
+ */
+@RestController
+@RequestMapping("/history")
+public class HistoryController {
+    
+    @Autowired
+    HistoryService historyService;
+    
+    /***
+     * @description 查询用户钱包金额变动明细
+     * @param uAccount 用户账号
+     * @return com.example.purse_demo.domain.response.ResponseData
+     * @author daorong
+     * @date 2023/3/1 14:08
+    */
+    @GetMapping("/{uAccount}")
+    public ResponseData getHistory(@PathVariable String uAccount){
+        return historyService.getHistory(uAccount);
+    }
+}
